dca66a1db408567bf516b1ebea50b4a0b62e2619
AntonA22 <antonabaluev2004@bk.ru>
2025-11-29 12:57:49 +0300

    do auth design

---
+++ Cafe-iOS-Anton/SceneDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  SceneDelegate.swift
+//  Cafe-ios
+//
+//  Created by Шаген  Арабачян on 29.11.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Создаем корневой контроллер
+        let rootViewController = AuthViewController()
+
+        // Получаем объект окна из сцены
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        // Создаем новое окно с использованием размеров сцены
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = rootViewController
+
+        // Устанавливаем новое окно как основное для сцены
+        self.window = window
+        window.makeKeyAndVisible()
+
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+
+    }
+}
