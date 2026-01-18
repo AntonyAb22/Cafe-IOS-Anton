792a56b3f0579d1d2a4b9f03c77f74eedf073bc5
AntonyAb22 <thecrew222@bk.ru>
2026-01-18 13:00:09 +0300

    integration grdb database

---
+++ Cafe-IOS-Anton/DataBase/GRDB.swift
@@ -0,0 +1,17 @@
+//
+//  GRDB.swift
+//  Cafe-iOS-Anton
+//
+//  Created by Антон Абалуев on 18.01.2026.
+//
+
+import Foundation
+import GRDB
+
+class GRDBDatabase {
+    private var dbQueue: DatabaseQueue?
+    
+    private func openDatabase() throws {
+        
+    }
+}
