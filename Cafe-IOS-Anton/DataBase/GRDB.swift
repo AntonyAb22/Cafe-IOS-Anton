b5503bf5319afbdae64bfdc85353647a8bafb9ca
AntonyAb22 <thecrew222@bk.ru>
2026-01-24 10:25:42 +0300

    create db database

--- Cafe-IOS-Anton/DataBase/GRDB.swift
+++ Cafe-IOS-Anton/DataBase/GRDB.swift
@@ -11,7 +11,26 @@ import GRDB
 class GRDBDatabase {
     private var dbQueue: DatabaseQueue?
     
+    init() {
+        do {
+            try openDatabase()
+            // дописать остальные сущности
+        } catch {
+            print("Ощибка при инициализация базы данных: \(error)")
+        }
+    }
+    
     private func openDatabase() throws {
+        let fileURL = try FileManager.default
+            .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            .appendingPathComponent("database.sqlite")
+        
+        dbQueue = try DatabaseQueue(path: fileURL.path)
         
     }
+}
+
+// ошибки базы данных
+enum DatabaseError: Error {
+    case databaseNotInitialized
 }
